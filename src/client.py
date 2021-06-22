b3b5845823796c436dd4cf176d68f08f62fd79a6
chronosss1 <djjmpro2000@gmail.com>
2021-06-22 22:06:25 +0200

    task4

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 import urllib.request
+from urllib import error
 import json
 
 ####################
@@ -14,13 +15,19 @@ TIMEOUT_REQUEST = 5  # in seconds
 class MusicClient:
     
     def __do_request(self, method, dir):
-        # Create the full address
         path = IP_ADDRESS + f":{str(PORT)}" + dir
+        print(path)
 
-        # Get data decoded
-        response = urllib.request.urlopen(path, timeout=TIMEOUT_REQUEST)
+        try:
+            response = urllib.request.urlopen(path, timeout=TIMEOUT_REQUEST)
+        except error.URLError as e:
+            print(e)
+            return None
+        except ConnectionRefusedError as e:
+            print(e)
+            return None
+
         data = json.loads(response.read().decode("utf-8")).get("data")
-        
         return data
 
     def get_avaliable_intervals(self):
@@ -28,10 +35,12 @@ class MusicClient:
 
     def get_interval_info(self, key):
         intervals = self.get_avaliable_intervals()
+
+        if intervals == None:
+            return None
         return intervals[key]
 
     def get_interval_songs(self, key, is_asc):
-        mode = "asc" if is_asc else "des"
-        dir = f"/songs/{key}/{mode}"
+        dir = f"/songs/{key}/{is_asc}"
 
         return self.__do_request("GET", dir)
--- src/ipm-p1.py
+++ src/ipm-p1.py
@@ -2,153 +2,131 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 
 import operator
+import time
+from threading import Thread
 
 import music
 from client import MusicClient
 
 class MusicWindow:
 
-    client = None
-    is_asc = True
+    mode = "asc"
+
+    retrying_connection = False     # Prevent from trying reconnect from various threads
+    
     elements_screen1 = []
     elements_screen2 = []
 
-    ####################
-    # PRIVATE METHODS
-    ####################
-
-    # Main
     def __init__(self, title="Default title", width=400, height=500, resizable=True):
         self.client = MusicClient()
 
         self.main_window = Gtk.Window(title=title, resizable=resizable)
         self.main_window.set_default_size(width, height)
         self.main_window.connect('delete-event', Gtk.main_quit)
+        self.main_window.show()
 
-    # Query
-    def search(self, key, is_asc):
+    ####################
+    # ACTIONS
+    ####################  
+
+    def search_intervals(self, *args):
+        data = self.client.get_avaliable_intervals()
+        connected = (data != None)
+
+        GLib.idle_add(self.update_infobar, connected)
+        if connected:
+            GLib.idle_add(self.update_interval_list, data)
+        else:
+            self.check_connectivity(self.search_intervals)
+
+    def search_songs(self, *args):
+        key, mode = args[0], args[1]
         info = self.client.get_interval_info(key)
-        songs = self.client.get_interval_songs(key, is_asc)
+        songs = self.client.get_interval_songs(key, mode)
+        
+        connected = (info != None) and (songs != None)
+        GLib.idle_add(self.update_infobar, connected)
 
-        text = f"{key}_{'asc' if is_asc else 'des'}"
-        self.search_entry.set_text(text)
-        self.text_result.set_markup(f'<i>Resultados para "{text}"</i>')
-
-        self.add_interval_info(self.stack, key, is_asc, info)
-        self.add_interval_songs(self.stack, songs)
-        self.set_screen(1)
+        if connected:
+            GLib.idle_add(self.update_results_data, f"{key}_{mode}", info, songs)
+        else:
+            self.check_connectivity(self.search_songs, *args)  # try to recconect
 
     def on_button_search(self, widget):
         label = widget.get_label()
         key = music.get_interval_key_by_full_name(label)
-        return self.search(key, self.is_asc)
+        Thread(target=self.search_songs, args=(key,self.mode), daemon=True).start()
 
     def on_entry_search(self, widget):
-        key, is_asc = self.validate_query(widget)
+        key, mode = self.validate_query(widget)
 
-        if key != None and is_asc != None:
-            self.search(key, is_asc == "asc")
-
-    def validate_query(self, widget):
-        text = widget.get_text().split("_")
-
-        widget.get_style_context().add_class("error")
-
-        if (text == [""]):
-            widget.get_style_context().remove_class("error")
-            return None, None
-
-        if (len(text) != 2):
-            return None, None
-
-        key, mode = text[0], text[1]
-        if key in music.ACRON_NAME_INTERVALS.keys() and mode in ("asc", "des"):
-            widget.get_style_context().remove_class("error")
-            return key, mode
-
-        return None, None
-
-    # Intervals list
-    def compare_intervals(self, row1, row2, fun):
-        interval1 = row1.get_children()[0].get_label()  # row->button->label
-        interval2 = row2.get_children()[0].get_label()
-
-        key_list = list(music.ACRON_NAME_INTERVALS)
-        key1 = music.get_interval_key_by_full_name(interval1)
-        key2 = music.get_interval_key_by_full_name(interval2)
-
-        return fun(key_list.index(key1), key_list.index(key2))
-
-    def add_intervals_buttons(self, intervals_list):
-        # Get avaliable intervals in the server
-        client = MusicClient()
-        avaliable_intervals = client.get_avaliable_intervals()
-
-        # Create each button
-        for key in avaliable_intervals:
-            row = Gtk.ListBoxRow(selectable=False, activatable=False)
-
-            button = Gtk.Button(label=music.get_interval_full_name(key))
-            button.set_margin_top(5)
-            button.connect("clicked", self.on_button_search)
-            row.add(button)
-
-            intervals_list.add(row)
-
-    # Result list
+        if key != None and mode != None:
+            Thread(target=self.search_songs, args=(key,mode), daemon=True).start()
+        
     def on_mas_canciones_clicked(self, widget, box, info):
         widget.destroy()
 
         for song in info:
             self.add_song_to_list(box, song[0], song[1], song[2], False)
 
-    def add_song_to_list(self, list_box, title, link, fav, first):
-        if not first:
-            separator = Gtk.Separator()
-            separator.show()
-            list_box.pack_start(separator, False, False, 10)
-
-        text = 'Título: '
-        if link != '':
-            text = text + '<a href="' + link +'"title="Abre el enlace en el navegador">' + title +'</a>' +'\n'
-        else:
-            text = text + f"{title} \n"
-        text = text + f'Favorita: {"SI" if fav == "YES" else "NO"}'
-
-        song_label = Gtk.Label()
-        song_label.set_halign(Gtk.Align.START)
-        song_label.set_markup(text)
-        song_label.show()
-        list_box.add(song_label)
-
-    # Moving through screens
-    def __on_return_clicked(self, widget, screen):
+    def on_return_clicked(self, widget, screen):
         self.set_screen(screen)
 
     ####################
-    # PUBLIC METHODS
+    # STATE
+    ####################
+    
+    def check_connectivity(self, fun, *args):
+        if self.retrying_connection:
+            return
+
+        self.retrying_connection = True
+        response = self.client.get_avaliable_intervals()
+
+        while response == None:
+            print("Cheking connectivity...")
+            response = self.client.get_avaliable_intervals()
+            time.sleep(0.5)
+
+        print("Connection established")
+        self.retrying_connection = False
+
+        GLib.idle_add(self.update_infobar, True)
+        fun(*args)  # Retry the operation that failed before
+
+    def set_interval_mode(self, widget, mode):
+        self.mode = mode
+
+    def set_sort_func(self, widget, fun):
+        self.intervals_list.set_sort_func(self.compare_intervals, fun)
+
+    ####################
+    # VIEW
     ####################
 
+    ##### Skeleton of the app
     def build(self):
         window_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, homogeneous=False, spacing=0)
         self.main_window.add(window_vbox)
+        window_vbox.show()
 
         # Info bar
         self.infobar = Gtk.InfoBar()
-        self.set_text_infobar(self.infobar, "Lo sentimos, el servicio no está disponible en estos momentos.")
 
-        self.infobar.set_show_close_button(True)
+        label = Gtk.Label(label="Lo sentimos, parece que hay un problema con el servidor")
+        self.infobar.get_content_area().add(label)
+        self.infobar.set_message_type(Gtk.MessageType.ERROR)
+
         self.infobar.connect("response", lambda widget,data: widget.hide())
-        
         window_vbox.pack_start(self.infobar, expand=False, fill=False, padding=0)
 
         # Body vbox
-        body_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, homogeneous=False)
-        body_vbox.set_border_width(30)
-        window_vbox.pack_start(body_vbox, expand=True, fill=True, padding=0)
+        self.body_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, homogeneous=False)
+        self.body_vbox.set_border_width(30)
+        window_vbox.pack_start(self.body_vbox, expand=True, fill=True, padding=0)
 
         # Return button
         return_button = Gtk.Button()
@@ -158,10 +136,10 @@ class MusicWindow:
         return_arrow = Gtk.Image.new_from_file("../images/return_arrow.png")
         return_button.set_image(return_arrow)
         
-        return_button.connect("clicked", self.__on_return_clicked, 0)
+        return_button.connect("clicked", self.on_return_clicked, 0)
 
         self.elements_screen2.append(return_button)
-        body_vbox.add(return_button)
+        self.body_vbox.add(return_button)
 
         # Search bar
         self.search_entry = Gtk.SearchEntry()
@@ -171,7 +149,7 @@ class MusicWindow:
         self.search_entry.connect("search-changed", self.validate_query)
         self.search_entry.connect("activate", self.on_entry_search)
         
-        body_vbox.pack_start(self.search_entry, expand=False, fill=False, padding=0)
+        self.body_vbox.pack_start(self.search_entry, expand=False, fill=False, padding=0)
 
         # Search bar result subtext
         self.text_result = Gtk.Label()
@@ -179,22 +157,22 @@ class MusicWindow:
         self.text_result.set_margin_bottom(20)
 
         self.text_result.set_halign(Gtk.Align.START)
-        body_vbox.pack_start(self.text_result, expand=False, fill=False, padding=5)
+        self.body_vbox.pack_start(self.text_result, expand=False, fill=False, padding=5)
         self.elements_screen2.append(self.text_result)
 
         # Selection text
         select_label = Gtk.Label()
         select_label.set_markup("<big>o selecciona</big>")
         self.elements_screen1.append(select_label)
-        body_vbox.pack_start(select_label, expand=False, fill=False, padding=20)
+        self.body_vbox.pack_start(select_label, expand=False, fill=False, padding=20)
 
         # Frame list container
-        self.frame = Gtk.Frame()
-        self.elements_screen1.append(self.frame)
-        body_vbox.pack_start(self.frame, expand=True, fill=True, padding=0)
+        frame = Gtk.Frame()
+        self.elements_screen1.append(frame)
+        self.body_vbox.pack_start(frame, expand=True, fill=True, padding=0)
 
         frame_box = Gtk.VBox(homogeneous=False, spacing=5)
-        self.frame.add(frame_box)
+        frame.add(frame_box)
 
         # Header box
         header_box = Gtk.Box(homogeneous=False)
@@ -206,8 +184,8 @@ class MusicWindow:
         descendent_button = Gtk.RadioButton.new_with_label_from_widget(ascendent_button, "Descendente")
         descendent_button.set_active(False)
 
-        ascendent_button.connect("clicked", self.set_interval_mode, True)
-        descendent_button.connect("clicked", self.set_interval_mode, False)
+        ascendent_button.connect("clicked", self.set_interval_mode, "asc")
+        descendent_button.connect("clicked", self.set_interval_mode, "des")
 
         header_box.add(ascendent_button)
         header_box.add(descendent_button)
@@ -234,8 +212,7 @@ class MusicWindow:
 
         self.intervals_list = Gtk.ListBox()
         self.intervals_list.set_sort_func(self.compare_intervals, operator.gt)
-        self.add_intervals_buttons(self.intervals_list)
-
+        Thread(target=self.search_intervals, daemon=True).start()
         scroll_window.add(self.intervals_list)
 
         # Results stack
@@ -267,42 +244,37 @@ class MusicWindow:
         songs_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         songs_window.add(songs_vbox)
 
-        body_vbox.pack_start(stacksw, False, False, 10)
-        body_vbox.pack_start(self.stack, True, True, 10)
-        
-    ####################
-    # SETTERS
-    ####################
+        self.body_vbox.pack_start(stacksw, False, False, 10)
+        self.body_vbox.pack_start(self.stack, True, True, 10)  
 
-    # Screen
-    def set_screen(self, screen):
-        self.main_window.show_all()
-        self.infobar.hide()
-
-        if screen == 0:
-            for element in self.elements_screen2:
-                element.hide()
+    ##### Infobar
+    def update_infobar(self, connected):
+        if connected:
+            self.infobar.hide()
         else:
-            for element in self.elements_screen1:
-                element.hide()
+            self.infobar.show_all()
 
-    # Infobar
-    def set_text_infobar(self, infobar, text, type=Gtk.MessageType.ERROR):
-        label = Gtk.Label(label=text)
-        infobar.get_content_area().add(label)
-        infobar.set_message_type(type)
+    ##### List of avaliable intervals
+    def update_interval_list(self, avaliable_intervals):
+        # Clear the current list
+        for element in self.intervals_list:
+            element.destroy()
 
-    # Header options
-    def set_interval_mode(self, widget, is_asc):
-        self.is_asc = is_asc
+        # Create each button
+        for key in avaliable_intervals:
+            row = Gtk.ListBoxRow(selectable=False, activatable=False)
 
-    # Intervals list
-    def set_sort_func(self, widget, fun):
-        self.intervals_list.set_sort_func(self.compare_intervals, fun)
+            button = Gtk.Button(label=music.get_interval_full_name(key))
+            button.set_margin_top(5)
+            button.connect("clicked", self.on_button_search)
+            row.add(button)
+            
+            row.show_all()
+            self.intervals_list.add(row)
 
-    # Result tabs
-    def add_interval_info(self, widget, key, is_asc, info):
-        box = widget.get_child_by_name("notas")
+    ##### List of notes
+    def add_interval_info(self, key, mode, info):
+        box = self.stack.get_child_by_name("notas")
         
         distance = box.get_children()[0]
         distance.set_markup(f"<i>Distancia: {info}</i>")
@@ -310,34 +282,52 @@ class MusicWindow:
         note1 = box.get_children()[1]
         note2 = box.get_children()[2]
 
-        sample1 = music.example_notes(key, is_asc)
+        sample1 = music.example_notes(key, mode)
         sample2 = sample1
         while sample2 == sample1:  # To not repeat the sample
-            sample2 = music.example_notes(key, is_asc)
+            sample2 = music.example_notes(key, mode)
 
         note1.set_markup(f"<big>{sample1[0]}-{sample1[1]}</big>")
         note2.set_markup(f"<big>{sample2[0]}-{sample2[1]}</big>")
 
-    def add_interval_songs(self, widget, info):
-        tab = widget.get_child_by_name("canciones")
+    ##### List of songs
+    def add_song_to_list(self, list_box, title, link, fav, first):
+        if not first:
+            separator = Gtk.Separator()
+            separator.show()
+            list_box.pack_start(separator, False, False, 10)
+
+        text = 'Título: '
+        if link != '':
+            text = text + '<a href="' + link +'"title="Abre el enlace en el navegador">' + title +'</a>' +'\n'
+        else:
+            text = text + f"{title} \n"
+        text = text + f'Favorita: {"SI" if fav == "YES" else "NO"}'
+
+        song_label = Gtk.Label()
+        song_label.set_halign(Gtk.Align.START)
+        song_label.set_markup(text)
+        song_label.show()
+        list_box.add(song_label)
+
+    def add_interval_songs(self, info):
+        tab = self.stack.get_child_by_name("canciones")
         box = tab.get_children()[0].get_children()[0]
 
         # Clear previous songs
         for obj in box:
             obj.destroy()
 
-        # Get a favourite songs
+        # Get a favourite song
         fav = None
         for song in info:
             if song[2] == "YES":
                 fav = song
                 break
-        
-        # If there are no favourite, choose the first one
-        if fav == None:
+    
+        if fav == None:  # If there are no favourite, choose the first one
             fav = info[0]
 
-        # Add to the box
         info.remove(fav)
         self.add_song_to_list(box, fav[0], fav[1], fav[2], True)
         
@@ -347,6 +337,75 @@ class MusicWindow:
 
         expand_button.connect('clicked', self.on_mas_canciones_clicked, box, info)
         box.pack_start(expand_button, False, False, 10)
+
+    def add_image_no_song(self):
+        tab = self.stack.get_child_by_name("canciones")
+        box = tab.get_children()[0].get_children()[0]
+
+        # Clear previous songs
+        for obj in box:
+            obj.destroy()
+
+        image = Gtk.Image.new_from_file("images/cara.png")
+        image.show()
+        box.add(image)
+
+    def update_results_data(self, query, info, songs):
+        self.search_entry.set_text(query)
+        self.text_result.set_markup(f'<i>Resultados para "{query}"</i>')
+
+        fields = query.split("_")
+        self.add_interval_info(fields[0], fields[1], info)
+
+        if (songs == [""]):
+            self.add_image_no_song()
+        else:
+            self.add_interval_songs(songs)
+
+        self.set_screen(1)
+
+    ##### Swap views
+    def set_screen(self, screen):
+        self.body_vbox.show_all()
+
+        if screen == 0:
+            for element in self.elements_screen2:
+                element.hide()
+        else:
+            for element in self.elements_screen1:
+                element.hide()
+
+    ####################
+    # UTILITIES
+    ####################
+
+    def validate_query(self, widget):
+        widget.get_style_context().add_class("error")
+
+        text = widget.get_text().split("_")
+        if (text == [""]):
+            widget.get_style_context().remove_class("error")
+            return None, None
+
+        if (len(text) != 2):
+            return None, None
+
+        key, mode = text[0], text[1]
+        if key in music.ACRON_NAME_INTERVALS.keys() and mode in ("asc", "des"):
+            widget.get_style_context().remove_class("error")
+            return key, mode
+
+        return None, None
+
+    def compare_intervals(self, row1, row2, fun):
+        interval1 = row1.get_children()[0].get_label()  # row->button->label
+        interval2 = row2.get_children()[0].get_label()
+
+        key_list = list(music.ACRON_NAME_INTERVALS)
+        key1 = music.get_interval_key_by_full_name(interval1)
+        key2 = music.get_interval_key_by_full_name(interval2)
+
+        return fun(key_list.index(key1), key_list.index(key2))
 
 
 if __name__ == "__main__":
